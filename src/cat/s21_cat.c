6dea47309d465c44a48fd10d14e0f93a3fe726b3
Genevieve Jenna <geneviej@et-l3.kzn.21-school.ru>
2023-11-14 21:42:19 +0300

    work cat

--- src/cat/s21_cat.c
+++ src/cat/s21_cat.c
@@ -1,23 +1,76 @@
-#include "s21_cat.h"
+#include <getopt.h>
+#include <stdbool.h>
+#include <stdio.h>
+typedef struct flags {
+  bool b;
+  bool e;
+  bool n;
+  bool s;
+  bool t;
+  bool v;
+  bool err;
+} flags;
 
-void reader(char *argv[],flags ) {
-  char buf[4096];
-  int read_bytes;
-  read_bytes = read(fd, buf, 4096);
-  while (read_bytes > 0) {
-    printf("%.*s", read_bytes, buf);
-    read_bytes = read(fd, buf, 4096);
+void print_symb(int c, int *prev, flags *info, int *index, bool *eline_printed);
+
+int reader(char *name, flags info) {
+  int err_code = 0;
+  FILE *f = fopen(name, "rt");
+  if (f != NULL) {
+    int index = 0;
+    bool eline_printed = false;
+    int c = fgetc(f), prev = '\n';
+    while (c != EOF) {
+      print_symb(c, &prev, &info, &index, &eline_printed);
+      c = fgetc(f);
+    }
+    fclose(f);
+  } else {
+    err_code = 1;
   }
+  return err_code;
 }
-void read_flags(flags *info, int argc, char *argv) {
-  flags info = {0};
+void print_symb(int c, int *prev, flags *info, int *index,
+                bool *eline_printed) {
+  if (info->s && *prev == '\n' && c == '\n' && *eline_printed) {
+    return;
+  }
+  if (*prev == '\n' && c == '\n') {
+    *eline_printed = true;
+  } else {
+    *eline_printed = false;
+  }
+  if (((info->n && !info->b) || (info->b && c != '\n')) && *prev == '\n') {
+    *index += 1;
+    printf("%6d\t", *index);
+  }
+  if (info->e && c == '\n') {
+    printf("$");
+  }
+  if (info->t && c == '\t') {
+    printf("^");
+    c = '\t' + 64;
+  }
+  if (info->v && c >= 0 && c <= 31 && c != '\n' && c != '\t') {
+    printf("^");
+    c = c + 64;
+  }
+  if (info->v && c == 127) {
+    if ((c = '?')) printf("^");
+  }
+  fputc(c, stdout);
+  // Обновляем предыдущий символ
+  *prev = c;
+}
+
+void read_flags(flags *info, int argc, char *argv[]) {
   static const struct option longFlags[] = {{"number-nonblank", 0, NULL, 'b'},
                                             {"number", 0, NULL, 'n'},
                                             {"squeeze-blank", 0, NULL, 's'},
                                             {NULL, 0, NULL, 0}};
-  int true_flags = getopt_long(argc, argv, "bevEnstT", longFlags, NULL);
-  for (; true_flags != -1;
-       true_flags = getopt_long(argc, argv, "bevEnstT", longFlags, NULL)) {
+  int true_flags;
+  while ((true_flags = getopt_long(argc, argv, "+bevEnstT", longFlags, NULL)) !=
+         -1) {
     switch (true_flags) {
       case 'b':
         info->b = true;
@@ -46,29 +99,23 @@ void read_flags(flags *info, int argc, char *argv) {
         info->v = true;
         break;
       default:
-        fprintf(stderr,"flag not found\n");
-        exit(1);
+        info->err = true;
     }
   }
 }
 
-/*bool args(int argc, char *argv[]){
-    flags info = {0};
-    for (int i = 1; i != argc ; ++i){
-        if (*argv[i] == '-'){
-           if (!read_flags(&info, argv[i]))
-            return false;
-        }
-    }
-
-    no_args(open(argv[1], O_RDONLY));
-}*/
-
 int main(int argc, char *argv[]) {
-  if (argc == 1)
-    no_args(STDIN_FILENO);
-  else {
-    if (args(argc, argv)) return 1;
-  }
+  flags info = {0};
+  read_flags(&info, argc, argv);
+  // Обрабатываем каждый файл, переданный в аргументах командной строки
+  if (!info.err) {
+    for (int i = 1; i < argc; i++) {
+      if ((!reader(argv[i], info) && i == argc)) {
+        fprintf(stderr, "s21_cat: %s: No such file or directory\n", argv[i]);
+      }
+    }
+  } else
+    fprintf(stderr, "usage: s21_cat [-benstv] [file ...]\n");
+
   return 0;
 }
